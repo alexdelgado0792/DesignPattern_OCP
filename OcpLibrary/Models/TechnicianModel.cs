dd036497b88fa85235bf52ce80e1ac8a9d174347
AlexDelgado0792 <arekkusu0792@gmail.com>
2019-03-27 11:58:48 -0600

    add new applicant

---
+++ OcpLibrary/Models/TechnicianModel.cs
@@ -0,0 +1,11 @@
+﻿using OcpLibrary.Interfaces;
+
+namespace OcpLibrary.Models
+{
+    public class TechnicianModel : IApplicantModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IAccounts AccountProcessor { get; set; } = new Accounts.Accounts();
+    }
+}
